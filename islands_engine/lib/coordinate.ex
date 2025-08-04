52d553d31811f06123c3295745fced15faccf677
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-04 21:36:19 +0700

    alias Coordinate function and use start link to start Agent.start_link airity two for a light weight create %Coordinate map state

---
+++ islands_engine/lib/coordinate.ex
@@ -0,0 +1,9 @@
+defmodule IslandsEngine.Coordinate do
+  defstruct in_island: :none, guessed?: false
+
+  alias IslandsEngine.Coordinate
+
+  def start_link do
+    Agent.start_link(fn -> %Coordinate{} end)
+  end
+end
