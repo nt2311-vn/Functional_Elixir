eda2c733a077b3ff43e18aead577f662a1abc2f5
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-05 21:11:35 +0700

    write function to check whether coordinate is hit or guessed?

--- islands_engine/lib/coordinate.ex
+++ islands_engine/lib/coordinate.ex
@@ -6,4 +6,39 @@ defmodule IslandsEngine.Coordinate do
   def start_link do
     Agent.start_link(fn -> %Coordinate{} end)
   end
+
+  @doc """
+  Function to return current value of key guessed? in Coordinate
+  """
+  @spec guessed?(Coordinate) :: boolean()
+  def guessed?(coordinate) do
+    Agent.get(coordinate, fn state -> state.guessed? end)
+  end
+
+  @doc """
+  Function to return currnet value of key in_island in Coordinate
+  """
+  @spec island(Coordinate) :: :none | any()
+  def island(coordinate) do
+    Agent.get(coordinate, fn state -> state.in_island end)
+  end
+
+  @doc """
+  Function to check whether Coordiante struct is island
+  """
+  @spec in_island?(Coordinate) :: boolean()
+  def in_island?(coordinate) do
+    case island(coordinate) do
+      :none -> false
+      _ -> true
+    end
+  end
+
+  @doc """
+  Function to check whether the Coordinate is hit
+  """
+  @spec hit?(Coordinate) :: boolean()
+  def hit?(coordinate) do
+    guessed?(coordinate) && in_island?(coordinate)
+  end
 end
