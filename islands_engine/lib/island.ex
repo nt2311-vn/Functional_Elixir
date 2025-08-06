d951f49dabbc0d080c2920518a548ed2ffb70718
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-06 20:52:13 +0700

    update function it should be function with airity one rather airity zero

--- islands_engine/lib/island.ex
+++ islands_engine/lib/island.ex
@@ -8,4 +8,13 @@ defmodule IslandsEngine.Island do
   def start_link() do
     Agent.start_link(fn -> [] end)
   end
+
+  @doc """
+  Function to replace a complete new list coordinate to
+  old one.
+  """
+  @spec replace_coordinates(list(Coordinate), list(Coordinate)) :: :ok
+  def replace_coordinates(island, new_coordinates) when is_list(new_coordinates) do
+    Agent.update(island, fn _state -> new_coordinates end)
+  end
 end
