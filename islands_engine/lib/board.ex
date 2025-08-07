d0767a65e8bc740882c9d3a60aa348a7ebfb8fd0
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-07 21:47:46 +0700

    write a bunch of functions to interact with position in a board update the value of coordinate

--- islands_engine/lib/board.ex
+++ islands_engine/lib/board.ex
@@ -34,4 +34,43 @@ defmodule IslandsEngine.Board do
   def get_coordinate(board, key) when is_atom(key) do
     Agent.get(board, fn board -> board[key] end)
   end
+
+  @doc """
+  Function to guess a coordiante by its position in board
+  """
+  @spec guess_coordinate(map, atom) :: :ok
+  def guess_coordinate(board, key) do
+    get_coordinate(board, key)
+    |> Coordinate.guess()
+  end
+
+  @doc """
+  Function to return whether a coordinate is hit
+  by its board position.
+  """
+  @spec coordinate_hit?(map, atom) :: boolean
+  def coordinate_hit?(board, key) do
+    get_coordinate(board, key)
+    |> Coordinate.hit?()
+  end
+
+  @doc """
+  Function to set a coordinate to a specific island
+  by its board position.
+  """
+  @spec set_coordinate_in_island(map, atom, atom) :: :ok
+  def set_coordinate_in_island(board, key, island) do
+    get_coordinate(board, key)
+    |> Coordinate.set_in_island(island)
+  end
+
+  @doc """
+  Function to return which island a coordinate is in
+  by ites board position.
+  """
+  @spec coordinate_island(map, atom) :: atom
+  def coordinate_island(board, key) do
+    get_coordinate(board, key)
+    |> Coordinate.island()
+  end
 end
